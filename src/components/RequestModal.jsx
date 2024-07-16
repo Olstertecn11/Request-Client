a14f0afa392b751c083662c639ec0299f0f08e35
Olstertecn11 <otzunund@miumg.edu.gt>
2024-07-15 20:04:09 -0600

    last update

--- src/components/RequestModal.jsx
+++ src/components/RequestModal.jsx
@@ -16,12 +16,24 @@ import {
   ModalCloseButton,
 } from '@chakra-ui/react'
 import { useState } from 'react';
+import axios from 'axios';
 
 const RequestModal = ({ isOpen, onClose }) => {
   const [anonymus, setAnonymus] = useState(true);
 
   const anonymusChange = (event) => {
     setAnonymus(event.target.checked);
+  }
+
+
+  const handleSend = async () => {
+    axios.post("http://localhost:3000/", {
+      "name": "Oliver",
+      "email": "olstertecn597@gmail.com",
+      "petition": "jfkldsajlfkasdklf"
+    }).then((response) => {
+      console.log(response);
+    });
   }
 
   return (
@@ -34,8 +46,8 @@ const RequestModal = ({ isOpen, onClose }) => {
         backdropFilter='blur(10px) hue-rotate(90deg)'
       />
       <ModalContent>
-        <ModalHeader color={'blue.700'} fontWeight='bold'>Mi Petición</ModalHeader>
-        <ModalCloseButton />
+        <ModalHeader bgColor={'#3182ce'} color={'white'} fontWeight='bold'>Mi Petición</ModalHeader>
+        <ModalCloseButton color='white' />
         <ModalBody pb={6}>
           <Flex align="center" mb={4} justify="flex-end">
             <FormLabel mb="0">¿Anónimo?</FormLabel>
@@ -48,13 +60,13 @@ const RequestModal = ({ isOpen, onClose }) => {
             </FormControl>
           )}
           <FormControl mt={4} >
-            <FormLabel>Contenido</FormLabel>
-            <Textarea placeholder='Pido al señor por...' bg='gray.200' />
+            <FormLabel color='#3182ce'>Contenido</FormLabel>
+            <Textarea placeholder='Pido al señor por...' bg='gray.200' padding={2} />
           </FormControl>
         </ModalBody>
 
         <ModalFooter>
-          <Button colorScheme='blue' mr={3}>
+          <Button colorScheme='blue' mr={3} onClick={handleSend}>
             Enviar Petición
           </Button>
           <Button onClick={onClose}>Cancelar</Button>
