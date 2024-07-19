9f97995b9fb77a9df3ea8ef341b89b822c64ef89
Olstertecn11 <otzunund@miumg.edu.gt>
2024-07-18 18:50:38 -0600

    admin section created

--- src/services/PetitionService.js
+++ src/services/PetitionService.js
@@ -10,6 +10,14 @@ const PetitionService = {
         "Authorization": "Bearer <token>" // Aquí deberías incluir tu token de autorización si lo tienes
       }
     }).then(res => res.data).catch(err => console.log(err));
+  },
+  all: async () => {
+    return axios.get(apiUrl, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer <token>" // Aquí deberías incluir tu token de autorización si lo tienes
+      }
+    }).then(res => res.data).catch(err => console.log(err));
   }
 }
 
